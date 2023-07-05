4696d3399d33160fec2f2bd085826f512fb749e0
priyam <priyamshankar.5@gmail.com>
2023-07-06 02:34:39 +0530

    main Dash page done without join and create cards

--- movie-trivia-client/src/App.js
+++ movie-trivia-client/src/App.js
@@ -8,7 +8,7 @@ import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div>
+    <div className="appcontainer">
       <Navbar/>
       <BrowserRouter>
         <Routes>
--- movie-trivia-client/src/pages/MainDash/MainDash.jsx
+++ movie-trivia-client/src/pages/MainDash/MainDash.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./style/Maindash.css";
+import dash from "../../Global/GlobalImages/dash.png"
 
 const MainDash = () => {
   const navigate = useNavigate();
-  const [Dialogbool, setDialogbool] = useState(false);
+  const [Dialogbool, setDialogbool] = useState("dash");
   const [roomId, setroomId] = useState(null);
   const [playerName, setplayerName] = useState(null);
 
@@ -58,9 +59,61 @@ const MainDash = () => {
       }
   }
 
+  function dialogBoxbuttonHandler(e){
+    setDialogbool(e.target.name);
+  }
+
   return (
+    <div className="mainDashContainer">
+        {Dialogbool=="dash"?<>
+      <div className="grid-comp-maindash">
+
+          <h1>
+          Play Dumbsharads with <span>Your</span> <br /> Friends in Real Time
+        </h1>
+        <h2>
+          Challange Your friends in a timed game of<br/> dumbsharads and chat with them in real time.
+        </h2>
+        <div className="button_container_MianDash">
+          <button className="maindashcreate" name="create" onClick={dialogBoxbuttonHandler}>Create Room</button>
+          <button className="maindashjoin" name="join" onClick={dialogBoxbuttonHandler}>Join Room</button>
+        </div>
+      </div>
+          </>:Dialogbool=="create"? <div className="create_mainDash">
+          <input
+          type="text"
+          placeholder="enter you name"
+          onChange={(e) => {
+            localStorage.setItem("playerName", e.target.value);
+            setplayerName(e.target.value);
+          }}
+          />
+          <input type="text" placeholder="Enter the room name" onChange={(e) => {
+              setroomId(e.target.value);
+            }}/>
+            <button onClick={handleCreateRoom}>create a Room</button>
+            <button
+            onClick={(e) => {
+              e.preventDefault();
+              setDialogbool(true);
+            }}
+            >
+            join a room
+            </button>
+          </div>: <div className="joinMainDash">
+          <input
+          type="text"
+          onChange={(e) => {
+            setroomId(e.target.value);
+          }}
+          />
+          <button onClick={handleJoinRoom}>Join Room</button>
+          </div> }
+      <div className="grid-pic-maindash">
+        <img src={dash} />
+      </div>
     <>
-      <div>
+      {/* <div>
         <input
           type="text"
           placeholder="enter you name"
@@ -68,15 +121,15 @@ const MainDash = () => {
             localStorage.setItem("playerName", e.target.value);
             setplayerName(e.target.value);
           }}
-        />
-      </div>
-      {Dialogbool && (
-        <div className="dialogBox">
+          />
+          </div>
+          {Dialogbool && (
+            <div className="dialogBox">
           <input
-            type="text"
-            onChange={(e) => {
-              setroomId(e.target.value);
-            }}
+          type="text"
+          onChange={(e) => {
+            setroomId(e.target.value);
+          }}
           />
           <button onClick={handleJoinRoom}>Join Room</button>
         </div>
@@ -86,18 +139,19 @@ const MainDash = () => {
           <input type="text" placeholder="Enter the room name" onChange={(e) => {
               setroomId(e.target.value);
             }}/>
-          <button onClick={handleCreateRoom}>create a Room</button>
-          <button
+            <button onClick={handleCreateRoom}>create a Room</button>
+            <button
             onClick={(e) => {
               e.preventDefault();
               setDialogbool(true);
             }}
-          >
+            >
             join a room
-          </button>
+            </button>
         </div>
-      )}
+      )} */}
     </>
+            </div>
   );
 };
 
