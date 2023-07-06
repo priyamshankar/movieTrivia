083d9a53a8f4f58b5560248a29591e410a75c0a9
priyam <priyamshankar.5@gmail.com>
2023-07-06 11:40:18 +0530

    dialog box for join and create styling done

--- movie-trivia-client/src/pages/MainDash/MainDash.jsx
+++ movie-trivia-client/src/pages/MainDash/MainDash.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./style/Maindash.css";
-import dash from "../../Global/GlobalImages/dash.png"
+import dash from "../../Global/GlobalImages/dash.png";
 
 const MainDash = () => {
   const navigate = useNavigate();
@@ -11,22 +11,22 @@ const MainDash = () => {
   const [playerName, setplayerName] = useState(null);
 
   async function isRoomValid() {
-    var state ;
+    var state;
     await axios
       .post("http://localhost:5000/api/checkroom", {
         room: roomId,
       })
       .then((res) => {
-        state=res.data;
+        state = res.data;
       });
-      return state;
+    return state;
   }
 
   async function handleJoinRoom(e) {
     if (roomId != null) {
-        const result = await isRoomValid();
-      if (result  === true) {
-        localStorage.setItem("admin",false);
+      const result = await isRoomValid();
+      if (result === true) {
+        localStorage.setItem("admin", false);
         navigate(`/game/${roomId}`);
       } else {
         alert("Room id dosen't exist Enter a valid one");
@@ -35,94 +35,145 @@ const MainDash = () => {
   }
 
   async function handleCreateRoom() {
-    if(playerName===null){
-        alert("first enter the player name");
-        return;
+    if (playerName === null) {
+      alert("first enter the player name");
+      return;
     }
     const result = await isRoomValid();
 
     if (result === true) {
-        alert("Room id already exist Enter another one");
-      } else {
-        axios.post("http://localhost:5000/api/addroom",{
-            room:roomId,
-            players:[
-                {
-                    player : playerName
-                }
-            ]
-        }).then((res)=>{
-            // console.log(res);
-            localStorage.setItem("admin",true);
-            navigate(`/game/${roomId}`);
+      alert("Room id already exist Enter another one");
+    } else {
+      axios
+        .post("http://localhost:5000/api/addroom", {
+          room: roomId,
+          players: [
+            {
+              player: playerName,
+            },
+          ],
         })
-      }
+        .then((res) => {
+          // console.log(res);
+          localStorage.setItem("admin", true);
+          navigate(`/game/${roomId}`);
+        });
+    }
   }
 
-  function dialogBoxbuttonHandler(e){
+  function dialogBoxbuttonHandler(e) {
     setDialogbool(e.target.name);
   }
 
   return (
     <div className="mainDashContainer">
-        {Dialogbool=="dash"?<>
-      <div className="grid-comp-maindash">
-
+      {Dialogbool == "dash" ? (
+        <>
+          <div className="grid-comp-maindash">
+            <h1>
+              Play Dumbsharads with <span>Your</span> <br /> Friends in Real
+              Time
+            </h1>
+            <h2>
+              Challange Your friends in a timed game of
+              <br /> dumbsharads and chat with them in real time.
+            </h2>
+            <div className="button_container_MianDash">
+              <button
+                className="maindashcreate"
+                name="create"
+                onClick={dialogBoxbuttonHandler}
+              >
+                Create Room
+              </button>
+              <button
+                className="maindashjoin"
+                name="join"
+                onClick={dialogBoxbuttonHandler}
+              >
+                Join Room
+              </button>
+            </div>
+          </div>
+        </>
+      ) : Dialogbool == "create" ? (
+        <div className="create_mainDash dash_next_menu">
           <h1>
-          Play Dumbsharads with <span>Your</span> <br /> Friends in Real Time
-        </h1>
-        <h2>
-          Challange Your friends in a timed game of<br/> dumbsharads and chat with them in real time.
-        </h2>
-        <div className="button_container_MianDash">
-          <button className="maindashcreate" name="create" onClick={dialogBoxbuttonHandler}>Create Room</button>
-          <button className="maindashjoin" name="join" onClick={dialogBoxbuttonHandler}>Join Room</button>
-        </div>
-      </div>
-          </>:Dialogbool=="create"? <div className="create_mainDash">
+            Create a Room to play with friends.
+          </h1>
           <input
-          type="text"
-          placeholder="enter you name"
-          onChange={(e) => {
-            localStorage.setItem("playerName", e.target.value);
-            setplayerName(e.target.value);
-          }}
-          />
-          <input type="text" placeholder="Enter the room name" onChange={(e) => {
-              setroomId(e.target.value);
-            }}/>
-            <button onClick={handleCreateRoom}>create a Room</button>
-            <button
-            onClick={(e) => {
-              e.preventDefault();
-              setDialogbool(true);
+            type="text"
+            placeholder="enter you name"
+            onChange={(e) => {
+              localStorage.setItem("playerName", e.target.value);
+              setplayerName(e.target.value);
             }}
-            >
-            join a room
+          />
+          <input
+            type="text"
+            placeholder="Enter the room name"
+            onChange={(e) => {
+              setroomId(e.target.value);
+            }}
+          />
+          <div className="button_container_MianDash">
+            <button onClick={handleCreateRoom} className="maindashcreate-btn">
+              create Room
             </button>
-          </div>: <div className="joinMainDash">
+            <button
+              className="maindashjoin-btn"
+              name="dash"
+              onClick={(e) => {
+                e.preventDefault();
+                dialogBoxbuttonHandler(e);
+              }}
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="joinMainDash dash_next_menu">
+          <h1>
+            Enter the Room id To join the Room
+          </h1>
           <input
-          type="text"
-          placeholder="enter you name"
-          onChange={(e) => {
-            localStorage.setItem("playerName", e.target.value);
-            setplayerName(e.target.value);
-          }}
+            type="text"
+            placeholder="enter you name"
+            onChange={(e) => {
+              localStorage.setItem("playerName", e.target.value);
+              setplayerName(e.target.value);
+            }}
           />
           <input
-          type="text"
-          onChange={(e) => {
-            setroomId(e.target.value);
-          }}
-          placeholder="Enter Room Id"
+            type="text"
+            onChange={(e) => {
+              setroomId(e.target.value);
+            }}
+            placeholder="Enter Room Id"
           />
-          <button onClick={handleJoinRoom}>Join Room</button>
-          </div> }
+          <div className="button_container_MianDash">
+            <button onClick={handleJoinRoom} className="maindashcreate-btn">
+              Join Room
+            </button>
+            <button
+              className="maindashjoin-btn"
+              name="dash"
+              onClick={(e) => {
+                e.preventDefault();
+                dialogBoxbuttonHandler(e);
+              }}
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      )}
       <div className="grid-pic-maindash">
         <img src={dash} />
       </div>
-    <>
-      {/* <div>
+      <>
+        {/* <div>
         <input
           type="text"
           placeholder="enter you name"
@@ -159,8 +210,8 @@ const MainDash = () => {
             </button>
         </div>
       )} */}
-    </>
-            </div>
+      </>
+    </div>
   );
 };
 
